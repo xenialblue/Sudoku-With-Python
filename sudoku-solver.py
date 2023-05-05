d5780db34af151a238c4f4bb24e5d0a50d214bb9
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-05-05 17:26:24 +0700

    Update sudoku-solver.py

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -39,3 +39,23 @@ def valid(bo, pos, num):
             
     return True
 
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)
+            
+    return None
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i !=0:
+            print("- - - - - - - - - -")
+        for j in range(len(bo[0])):
+            if j % 3 == 0:
+                print(" | ", end="")
+                
+            if j == 8:
+                print(bo[i][j], end="\n")
+            else:
+                print(str(bo[i][j]) + " ", end="")
