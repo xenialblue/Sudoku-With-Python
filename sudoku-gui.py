017b300d3c3b3555c5518d7fc9325b38a1581d6c
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-05-05 10:02:38 +0700

    Update sudoku-gui.py

--- sudoku-gui.py
+++ sudoku-gui.py
@@ -96,3 +96,88 @@ class Grid:
             return True
         else:
             row, col = find
+        
+        for i in range(1, 10):
+            if valid(self.model, i, (row,col)):
+                self.model[row][col] = i
+                
+                if self.solve():
+                    return True
+                
+                self.model[row][col] = 0
+                
+        return False
+    
+    def solve_gui(self):
+        self.update_model()
+        find = find_empty(self.model)
+        if not find:
+            return True
+        else:
+            row, col = find
+            
+        for i in range(1, 10):
+            if valid(self.model, i, (row, col)):
+                self.model[row][col] = i
+                self.cubes[row][col].set(i)
+                self.cubes[row][col].draw_change(self.win, True)
+                self.update_model()
+                pygame.display.update()
+                pygame.time.delay(100)
+                
+        return False
+    
+class Cube:
+    rows = 9
+    cols = 9
+    
+    def __init__(self, value, row, col, width, height):
+        self.value = value
+        self.temp = 0
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+        
+    def draw(self, win):
+        fnt = pygame.font.Sysfont("comicsans", 40)
+        
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+        
+        if self.temp != 0 and self.value == 0:
+            text = fnt.render(str(self.temp), 1, (128, 128, 128))
+            win.blit(text, (x+5, y+5))
+        elif not(self.value == 0):
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
+            win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
+            
+        if self.selected:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+            
+    def draw_change(self, win, g = True):
+        fnt = pygame.font.Sysfont("comicsans", 40)
+        
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+        
+        pygame.draw.rect(win, (255, 255, 255), (x, y, gap, gap), 0)
+        
+        text = fnt.render(str(self.value), 1, (0, 0, 0))
+        win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+        if g:
+            pygame.draw.rect(win, (0, 255, 0), (x, y, gap, gap), 3)
+        else:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+            
+    def set(self, val):
+        self.value = val
+            
+    def set_temp(self, val):
+        self.temp = val
+            
+    def find_empty(bo):
+        
