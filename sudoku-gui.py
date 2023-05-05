02aa6e9c620f214b496603eacfb4d474ef4af503
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-05-05 17:00:25 +0700

    update solver + GUI

--- sudoku-gui.py
+++ sudoku-gui.py
@@ -48,6 +48,7 @@ class Grid:
         self.cubes[row][col].set_temp(val)
         
     def draw(self):
+        #draw grid lines
         gap = self.width / 9
         for i in range(self.rows+1):
             if i % 3 == 0 and i != 0:
@@ -57,11 +58,13 @@ class Grid:
             pygame.draw.line(self.win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
             pygame.draw.line(self.win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
             
+        #draw cubes
         for i in range(self.rows):
             for j in range(self.cols):
                 self.cubes[i][j].draw(self.win)
                 
     def select(self, row, col):
+        #reset all other
         for i in range(self.rows):
             for j in range(self.cols):
                 self.cubes[i][j].selected = False
@@ -141,7 +144,7 @@ class Cube:
         self.selected = False
         
     def draw(self, win):
-        fnt = pygame.font.Sysfont("comicsans", 40)
+        fnt = pygame.font.SysFont("comicsans", 40)
         
         gap = self.width / 9
         x = self.col * gap
@@ -158,7 +161,7 @@ class Cube:
             pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
             
     def draw_change(self, win, g = True):
-        fnt = pygame.font.Sysfont("comicsans", 40)
+        fnt = pygame.font.SysFont("comicsans", 40)
         
         gap = self.width / 9
         x = self.col * gap
@@ -188,13 +191,131 @@ class Cube:
         return None
     
     def valid(bo, num, pos):
+        #check row
         for i in range(len(bo[0])):
             if bo[pos[0]][i] == num and pos[1] != i:
                 return False
             
+        #check column
         for i in range(len(bo)):
             if bo[i][pos[1]] == num and pos[0] != i:
                 return False
             
+        #check box    
         box_x = pos[1] // 3
         box_y = pos[0] // 3
+        
+        for i in range(box_y * 3, box_y * 3 + 3):
+            for j in range(box_x * 3, box_x * 3 + 3):
+                if bo[i][j] == num and (i, j) != pos:
+                    return False
+                
+        return True
+    
+    def redraw_window(win, board, time, strikes):
+        win.fill((255, 255, 255))
+        #draw Time
+        fnt = pygame.font.SysFont("comicsans", 40)
+        text = fnt.render("Time: " + format_time(time), 1, (0, 0, 0))
+        win.blit(text, (540 - 160, 560))
+        #draw strikes
+        text = fnt.render("X " * strikes, 1, (255, 0, 0))
+        win.blit(text, (20, 500))
+        #draw grid and board
+        board.draw()
+        
+    def format_time(secs):
+        sec = secs%60
+        minute = secs//60
+        hour = minute//60
+        
+        mat = " " + str(minute) + ":" + str(sec)
+        return mat
+    
+    def main():
+        win = pygame.display.set_mode((540, 600))
+        pygame.display.set_caption("Sudoku")
+        board = Grid(9, 9, 540, 540, win)
+        key = None
+        run = True
+        start = time.time()
+        strikes = 0
+        while run:
+            
+            play_time = round(time.time() - start)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        key = 1
+                    if event.key == pygame.K_2:
+                        key = 2
+                    if event.key == pygame.K_3:
+                        key = 3
+                    if event.key == pygame.K_4:
+                        key = 4
+                    if event.key == pygame.K_5:
+                        key = 5
+                    if event.key == pygame.K_6:
+                        key = 6
+                    if event.key == pygame.K_7:
+                        key = 7
+                    if event.key == pygame.K_8:
+                        key = 8
+                    if event.key == pygame.K_9:
+                        key = 9
+                    if event.key == pygame.K_KP1:
+                        key = 1
+                    if event.key == pygame.K_KP2:
+                        key = 2
+                    if event.key == pygame.K_KP3:
+                        key = 3
+                    if event.key == pygame.K_KP4:
+                        key = 4
+                    if event.key == pygame.K_KP5:
+                        key = 5
+                    if event.key == pygame.K_KP6:
+                        key = 6
+                    if event.key == pygame.K_KP7:
+                        key = 7
+                    if event.key == pygame.K_KP8:
+                        key = 8
+                    if event.key == pygame.K_KP9:
+                        key = 9
+                    if event.key == pygame.K_DELETE:
+                        board.clear()
+                        key = None
+                        
+                    if event.key == pygame.K_SPACE:
+                        board.solve_gui()
+                        
+                    if event.key == pygame.K_RETURN:
+                        i, j = board.selected
+                        if board.cubes[i][j].temp != 0:
+                            if board.place(board.cubes[i][j].temp):
+                                print("Success")
+                            else:
+                                print("Wrong")
+                                strikes += 1
+                            key = None
+                            
+                            if board.is_fininshed():
+                                print("Game Over")
+                                
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    clicked = board.click(pos)
+                    if clicked : 
+                        board.select(clicked[0], clicked[1])
+                        key = None
+                        
+            if board.selected and key != None:
+                board.sketch(key)
+                
+            redraw_window(win, board, play_time, strikes)
+            pygame.display.update()
+            
+main()
+pygame.quit()
